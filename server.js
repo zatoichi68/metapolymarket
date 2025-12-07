42718d8245c4e451cce73db9dd026712030d48ef
Steve Rioux <steverioux68@gmail.com>
2025-12-07 00:43:23 -0500

    Fix Cloud Run start: use global fetch instead of node-fetch

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import fetch from 'node-fetch';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
